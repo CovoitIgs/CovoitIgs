470000cddd3f4f4dc99f67e41ff1ff5ab4eb9c47
DAULY Robin <robin.dauly@campus-igs-toulouse.fr>
2017-04-27 16:15:49 +0200

    zoom maps

--- src/app/add-ride/add-ride.component.ts
+++ src/app/add-ride/add-ride.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./add-ride.component.scss']
 })
 export class AddRideComponent implements OnInit {
-    private lat = 0
-    private lng = 0
+    private lat = 43.600531
+    private lng = 1.440456
 
     ngOnInit() {
         if (navigator.geolocation) {
