66e6c97870fc8106e23317344f1d52ba13603ebe
POMAREL Quentin <quentin.pomarel@campus-igs-toulouse.fr>
2017-04-27 14:24:38 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component'
+import { ConnectionComponent } from './connection/connection.component'
+import { AddRideComponent } from './add-ride/add-ride.component'
 
 const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'connect', component: ConnectionComponent },
+    { path: 'rides/add', component: AddRideComponent }
 ]
 
 export default routes
