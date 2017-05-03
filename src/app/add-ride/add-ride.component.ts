b90806f66c4ad22fa1b5eaf1671faa3717817e80
MadDeveloper <sergent.julien@icloud.com>
2017-05-04 00:04:26 +0200

    feat(AddRide): Implement error handling

--- src/app/add-ride/add-ride.component.ts
+++ src/app/add-ride/add-ride.component.ts
@@ -8,6 +8,17 @@ import { Component, OnInit } from '@angular/core'
 export class AddRideComponent implements OnInit {
     private lat = 43.600531
     private lng = 1.440456
+    private departure = ''
+    private departureTime = ''
+    private arrival = ''
+    private arrivalTime = ''
+    private detour = ''
+    private recurrence = ''
+    private places = ''
+    private errorDeparture = false
+    private errorDepartureTime = false
+    private errorArrival = false
+    private errorArrivalTime = false
 
     ngOnInit() {
         if (navigator.geolocation) {
@@ -16,5 +27,49 @@ export class AddRideComponent implements OnInit {
                 this.lng = position.coords.longitude
             })
         }
+    }
+
+    route() {
+
+    }
+
+    add() {
+        if (this.rideValidation()) {
+            // add ride
+        }
+    }
+
+    private rideValidation() {
+        let valid = true
+
+        if (this.departure.length === 0) {
+            this.errorDeparture = true
+            valid = false
+        } else {
+            this.errorDeparture = false
+        }
+
+        if (this.departureTime.length === 0) {
+            this.errorDepartureTime = true
+            valid = false
+        } else {
+            this.errorDepartureTime = false
+        }
+
+        if (this.arrival.length === 0) {
+            this.errorArrival = true
+            valid = false
+        } else {
+            this.errorArrival = false
+        }
+
+        if (this.arrivalTime.length === 0) {
+            this.errorArrivalTime = true
+            valid = false
+        } else {
+            this.errorArrivalTime = false
+        }
+
+        return valid
     }
 }
