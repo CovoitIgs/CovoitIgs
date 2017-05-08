5bd1fa1a5579ac70dabcd4f5d3fa871a5ebc3a2c
Quentin <quentinpomarel@gmail.com>
2017-05-08 14:33:43 +0200

    link changed in home page + add maps to trip-details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, DoCheck } from '@angular/core'
+import { AuthenticationService } from '../services/authentication.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
+
+    isAuthenticated: boolean
 
   constructor() { }
 
   ngOnInit() {
-  }
+
+        this.isAuthenticated = AuthenticationService.logged()
+    }
+
+    ngDoCheck() {
+        this.isAuthenticated = AuthenticationService.logged()
+    }
 
 }
