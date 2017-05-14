1b15c1a0d70ba33ad43a3aad8d6f50e6c3ab1acc
Quentin <quentinpomarel@gmail.com>
2017-05-14 16:49:38 +0200

    Add trip ride on maps in trip-detail view

--- src/app/trip-details/trip-details.component.ts
+++ src/app/trip-details/trip-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { Trip } from 'app/interfaces/trip'
 
+declare var google: any
+
 @Component({
   selector: 'app-trip-details',
   templateUrl: './trip-details.component.html',
@@ -8,13 +10,41 @@ import { Trip } from 'app/interfaces/trip'
 })
 export class TripDetailsComponent implements OnInit {
 
-    private lat = 43.600531
-    private lng = 1.440456
+    private latDefault = 43.600531
+    private lngDefault = 1.440456
+
     @Input()
     private trip: Trip
 
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+
+    const directionsService = new google.maps.DirectionsService
+       const directionsDisplay = new google.maps.DirectionsRenderer
+       const map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: this.latDefault, lng: this.lngDefault}
+        })
+        directionsDisplay.setMap(map)
+        calculateAndDisplayRoute(directionsService, directionsDisplay)
+
+        function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+            directionsService.route({
+                origin: {lat: 43.840098, lng: 1.389654},
+                destination: {lat: 43.643841, lng: 1.386883},
+                travelMode: 'DRIVING'
+            }, function(response, status) {
+                if (status === 'OK') {
+                    directionsDisplay.setDirections(response)
+                } else {
+                    window.alert('Directions request failed due to ' + status)
+                }
+            })
+        }
+
+    }
+
 
 }
